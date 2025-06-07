3ba198a3b0cb419cc87aa785bc9e58219a5cd8f0
nixzhu <zhuhongxu@gmail.com>
2025-06-07 08:39:05 +0800

    add Dictionary decode API

--- Sources/Banana/BananaModel.swift
+++ Sources/Banana/BananaModel.swift
@@ -51,6 +51,52 @@ extension BananaModel {
     }
 }
 
+extension Dictionary where Key == String, Value: BananaModel {
+    public static func decode(
+        from jsonData: Data,
+        path: [BananaPathItem] = []
+    ) -> Self {
+        let doc = jsonData.withUnsafeBytes {
+            yyjson_read($0.bindMemory(to: CChar.self).baseAddress, jsonData.count, 0)
+        }
+
+        if let doc {
+            defer {
+                yyjson_doc_free(doc)
+            }
+
+            var json = BananaJSON(
+                pointer: yyjson_doc_get_root(doc)
+            )
+
+            for item in path {
+                switch item {
+                case .key(let key):
+                    json = json[key]
+                case .index(let index):
+                    json = json[index]
+                }
+            }
+
+            return json.dictionary().mapValues { .init(json: $0) }
+        } else {
+            return [:]
+        }
+    }
+
+    public static func decode(
+        from jsonString: String,
+        path: [BananaPathItem] = [],
+        encoding: String.Encoding = .utf8
+    ) -> Self {
+        if let jsonData = jsonString.data(using: encoding) {
+            decode(from: jsonData, path: path)
+        } else {
+            [:]
+        }
+    }
+}
+
 extension Array where Element: BananaModel {
     public static func decode(
         from jsonData: Data,
---
+++ Tests/BananaTests/DictionaryTests.swift
@@ -0,0 +1,66 @@
+import Testing
+@testable import Banana
+
+@Test func dictionary1() {
+    struct Item: BananaModel {
+        let id: Int
+        let name: String
+
+        init(json: BananaJSON) {
+            id = json.id.int()
+            name = json.name.string()
+        }
+    }
+
+    let jsonData = """
+        {
+            "a": {
+                "id": 0,
+                "name": "nix"
+            },
+            "b": {
+                "id": 1,
+                "name": "zhu"
+            }
+        }
+        """.data(using: .utf8)!
+
+    let info = [String: Item].decode(from: jsonData)
+    #expect(info["a"]?.id == 0)
+    #expect(info["a"]?.name == "nix")
+    #expect(info["b"]?.id == 1)
+    #expect(info["b"]?.name == "zhu")
+}
+
+@Test func dictionary2() {
+    struct Item: BananaModel {
+        let id: Int
+        let name: String
+
+        init(json: BananaJSON) {
+            id = json.id.int()
+            name = json.name.string()
+        }
+    }
+
+    let jsonData = """
+        {
+            "x": {
+                "a": {
+                    "id": 0,
+                    "name": "nix"
+                },
+                "b": {
+                    "id": 1,
+                    "name": "zhu"
+                }
+            }
+        }
+        """.data(using: .utf8)!
+
+    let info = [String: Item].decode(from: jsonData, path: ["x"])
+    #expect(info["a"]?.id == 0)
+    #expect(info["a"]?.name == "nix")
+    #expect(info["b"]?.id == 1)
+    #expect(info["b"]?.name == "zhu")
+}
