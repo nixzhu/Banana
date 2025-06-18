08f8d89e160517c2b73b326dac5f91ef9b1d8f91
nixzhu <zhuhongxu@gmail.com>
2025-06-18 12:37:10 +0800

    better dependencies

--- Package.swift
+++ Package.swift
@@ -2,46 +2,6 @@
 
 import PackageDescription
 
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS) || os(visionOS)
-let packageDependencies: [Package.Dependency] = [
-    .package(
-        url: "https://github.com/ibireme/yyjson.git",
-        from: "0.11.1"
-    ),
-    .package(
-        url: "https://github.com/michaeleisel/JJLISO8601DateFormatter.git",
-        from: "0.1.8"
-    ),
-]
-#else
-let packageDependencies: [Package.Dependency] = [
-    .package(
-        url: "https://github.com/ibireme/yyjson.git",
-        from: "0.11.1"
-    ),
-]
-#endif
-
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS) || os(visionOS)
-let targetDependencies: [Target.Dependency] = [
-    .product(
-        name: "yyjson",
-        package: "yyjson"
-    ),
-    .product(
-        name: "JJLISO8601DateFormatter",
-        package: "JJLISO8601DateFormatter"
-    ),
-]
-#else
-let targetDependencies: [Target.Dependency] = [
-    .product(
-        name: "yyjson",
-        package: "yyjson"
-    ),
-]
-#endif
-
 let package = Package(
     name: "Banana",
     products: [
@@ -50,11 +10,47 @@ let package = Package(
             targets: ["Banana"]
         ),
     ],
-    dependencies: packageDependencies,
+    dependencies: {
+        var result: [Package.Dependency] = [
+            .package(
+                url: "https://github.com/ibireme/yyjson.git",
+                from: "0.11.1"
+            ),
+        ]
+
+        #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS) || os(visionOS)
+        result.append(
+            .package(
+                url: "https://github.com/michaeleisel/JJLISO8601DateFormatter.git",
+                from: "0.1.8"
+            )
+        )
+        #endif
+
+        return result
+    }(),
     targets: [
         .target(
             name: "Banana",
-            dependencies: targetDependencies
+            dependencies: {
+                var result: [Target.Dependency] = [
+                    .product(
+                        name: "yyjson",
+                        package: "yyjson"
+                    ),
+                ]
+
+                #if os(macOS) || os(iOS) || os(watchOS) || os(tvOS) || os(visionOS)
+                result.append(
+                    .product(
+                        name: "JJLISO8601DateFormatter",
+                        package: "JJLISO8601DateFormatter"
+                    )
+                )
+                #endif
+
+                return result
+            }()
         ),
         .testTarget(
             name: "BananaTests",
